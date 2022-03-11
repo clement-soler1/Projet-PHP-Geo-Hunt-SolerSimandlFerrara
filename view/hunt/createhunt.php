cb5a51098b9061b53cc6a7399c267ec897966a63
Ryan Simandl <ryan.simandl@gmail.com>
2022-03-11 14:42:58 +0100

    remove mapbox

--- view/hunt/createhunt.php
+++ view/hunt/createhunt.php
@@ -1,4 +1,3 @@
-<script>require('leaflet.js');</script>
 <link rel="stylesheet" type="text/css" href="./assets/css/login.css">
 <section class="login-dark">
     <form method="post" action="">
@@ -7,7 +6,7 @@
         <div class="mb-3"><input class="form-control" type="text" name="title" placeholder="title"></div>
         <div class="mb-3" style="display: flex; flex-direction: row">
             <p>private</p>
-            <input type="checkbox" name="privacy" placeholder="privacy" style="margin-left: 25vh">
+            <input type="checkbox" name="privacy" placeholder="privacy" style="margin-left: 30vh">
         </div>
         <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Create</button></div>
         <a class="forgot" href="?action=createquestion&controller=question">No questions? create some!</a>
@@ -15,12 +14,14 @@
         <input type="hidden" name="controller" value="user">
     </form>
 
-    <div id='map'></div>
+    <div id='map' style="width: 50vh; height: 50vh"></div>
     <script>
-        L.mapbox.accessToken = 'pk.eyJ1IjoidGFnZ2VyMDYiLCJhIjoiY2wwbWV5ZzFvMDl5bjNjbnRvcXpydjBmaSJ9.qR8ING20xL9FR5mapnRO3A';
-        var map = L.mapbox.map('map')
-            .setView([40, -74.50], 9)
-            .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-74.5, 40], // starting position [lng, lat]
+            zoom: 9 // starting zoom
+        });
     </script>
 
 </section>
