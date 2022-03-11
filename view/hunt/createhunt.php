ea42ef8e878694b061bb45b0bc1e882aa9e6fe8b
Ryan Simandl <ryan.simandl@gmail.com>
2022-03-11 16:39:20 +0100

    create hunt avec map
    il manque la selection de point sur la map

--- view/hunt/createhunt.php
+++ view/hunt/createhunt.php
@@ -1,21 +1,23 @@
-<link rel="stylesheet" type="text/css" href="./assets/css/login.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/hunt.css">
 <section class="login-dark">
     <form method="post" action="">
-        <h2 class="visually-hidden">Signup Form</h2>
+        <h2 class="visually-hidden">Créer une piste</h2>
         <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
         <div class="mb-3"><input class="form-control" type="text" name="title" placeholder="title"></div>
         <div class="mb-3" style="display: flex; flex-direction: row">
-            <p>private</p>
-            <input type="checkbox" name="privacy" placeholder="privacy" style="margin-left: 30vh">
+            <p style="margin-right: 15vh;margin-bottom: 0.5rem">piste privée :</p>
+            <input type="checkbox" name="privacy" placeholder="privacy">
         </div>
+        <p>début de piste :</p>
+        <div id='map'></div>
         <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Create</button></div>
         <a class="forgot" href="?action=createquestion&controller=question">No questions? create some!</a>
         <input type="hidden" name="action" value="create">
         <input type="hidden" name="controller" value="user">
     </form>
 
-    <div id='map' style="width: 50vh; height: 50vh"></div>
     <script>
+        mapboxgl.accessToken = '<?php echo $MAPBOX_TOKEN;?>';
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
